41cc577de72f3846748cda7c77b762153ce55141
Andre Palacios Duran <andrepalacios6991@gmail.com>
2025-07-03 17:46:55 -0400

    changed dirs in new script to relative paths

--- stimuli/code/calculate_duration_corrective_factors.m
+++ stimuli/code/calculate_duration_corrective_factors.m
@@ -1,10 +1,11 @@
 % 64s is known duration of the original stimuli
 %% get durations of wavs in folder:
 clear, close all
-
+%%
 % select which audio set to look at
-fldr='C:\Users\apalaci6.URMC-SH\Box\my box\LALOR LAB\oscillations project\MATLAB\Warped Speech\stimuli\wrinkle\stretchy_compressy_temp\stretchy_irreg\rule2_seg_bark_median_unnormalized_durations';
-% fldr='C:\Users\apalaci6.URMC-SH\Box\my box\LALOR LAB\oscillations project\MATLAB\Warped Speech\stimuli\wrinkle\stretchy_compressy_temp\stretchy_irreg\rule7_seg_bark_median_unnormalized_durations';
+global boxdir_mine
+fldr=[boxdir_mine '\stimuli\wrinkle\stretchy_compressy_temp\stretchy_irreg\rule2_seg_bark_median_unnormalized_durations'];
+% fldr=[boxdir_mine '\stimuli\wrinkle\stretchy_compressy_temp\stretchy_irreg\rule7_seg_bark_median_unnormalized_durations'];
 D=dir([fldr, '\*.wav']);
 durations=zeros(size(D));
 for dd=1:numel(D)
@@ -21,5 +22,5 @@ fprintf('avg dur: %0.3f\n',mean(durations))
 
 interval_factor=durations./64;
 corrected_durations=durations./interval_factor;
-global boxdir_mine
+
 output_fldr=[boxdir_mine '\stimuli'];
